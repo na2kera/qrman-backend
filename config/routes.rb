5dd2c51ec771d429c0d38876029cae737c51443d
na2kera <nat3mitaro@gmail.com>
2025-06-27 01:06:45 +0900

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get "protected_resource/index"
+      get "sessions/create"
+      get "users/create"
+    end
+  end
   post "auth/:provider/callback", to: "api/v1/users#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get create" do
+    get api_v1_users_create_url
+    assert_response :success
+  end
 end
