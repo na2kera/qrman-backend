1fa194923a19dd29d93a5e2e701afa4c6e67f688
na2kera <nat3mitaro@gmail.com>
2025-08-15 00:21:17 +0900

    画像URL

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,3 +1,11 @@
 class Website < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   belongs_to :user
+
+  has_one_attached :image
+  
+  def image_url
+    image.attached? ? url_for(image) : nil
+  end
 end
