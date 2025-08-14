596f8172b29063c2c0f31451be9970ac470ec1d2
na2kera <nat3mitaro@gmail.com>
2025-08-15 00:13:56 +0900

    websiteテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_26_155619) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_14_150247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,4 +20,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_26_155619) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "websites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_websites_on_user_id"
+  end
+
+  add_foreign_key "websites", "users"
 end
