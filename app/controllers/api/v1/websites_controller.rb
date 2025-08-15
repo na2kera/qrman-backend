8a266a158441664ecf5aa2aabe45465f0b2bc710
na2kera <nat3mitaro@gmail.com>
2025-08-16 02:22:15 +0900

    リンターエラー修正

--- app/controllers/api/v1/websites_controller.rb
+++ app/controllers/api/v1/websites_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::WebsitesController < ApplicationController
   def index
     websites = Website.all
-    render json: websites.as_json(methods: [:image_url])
+    render json: websites.as_json(methods: [ :image_url ])
   end
 
   def show
     website = Website.find(params[:id])
-    render json: website.as_json(methods: [:image_url])
+    render json: website.as_json(methods: [ :image_url ])
   end
 
   def create
     website = Website.new(website_params.except(:image))
     website.image.attach(website_params[:image]) if website_params[:image].present?
     website.save!
-    render json: website.as_json(methods: [:image_url])
+    render json: website.as_json(methods: [ :image_url ])
   end
 
   def update
@@ -24,7 +24,7 @@ class Api::V1::WebsitesController < ApplicationController
       website.image.purge_later if website.image.attached?
       website.image.attach(website_params[:image])
     end
-    render json: website.as_json(methods: [:image_url])
+    render json: website.as_json(methods: [ :image_url ])
   end
 
   def destroy
--- app/models/website.rb
+++ app/models/website.rb
@@ -4,7 +4,7 @@ class Website < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image, dependent: :purge_later # DBの制約では削除できないのでアプリケーションで面倒を見る
-  
+
   def image_url
     image.attached? ? Rails.application.routes.url_helpers.rails_blob_url(image) : nil
   end
