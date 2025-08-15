0a30924e6058c64162c4417692b2653a097a762c
na2kera <nat3mitaro@gmail.com>
2025-08-16 02:15:35 +0900

    画像削除

--- app/controllers/api/v1/websites_controller.rb
+++ app/controllers/api/v1/websites_controller.rb
@@ -19,11 +19,18 @@ class Api::V1::WebsitesController < ApplicationController
   def update
     website = Website.find(params[:id])
     website.update!(website_params.except(:image))
-    website.image.attach(website_params[:image]) if website_params[:image].present?
+    if website_params[:image].present?
+      # Replace existing image with the new one
+      website.image.purge_later if website.image.attached?
+      website.image.attach(website_params[:image])
+    end
     render json: website.as_json(methods: [:image_url])
   end
 
   def destroy
+    website = Website.find(params[:id])
+    website.destroy!
+    head :no_content
   end
 
   def website_params
